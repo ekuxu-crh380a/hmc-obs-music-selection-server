c275bab1c73ac489a3e447dd592622b199925e05
3.80GHz <shinra.dx@outlook.com>
2024-07-17 19:21:08 +0800

    定义曲库查询函数

---
+++ app/libraries/maimusic.py
@@ -0,0 +1,10 @@
+from app.settings import DATA_DIR
+import json
+
+
+def id2music(id):
+    with open(f"{DATA_DIR}/music_data.json", 'r', encoding='utf-8') as file:
+        music_data = json.load(file)
+    for music in music_data:
+        if id == music["id"]:
+            return music
