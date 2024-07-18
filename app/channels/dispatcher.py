e371828bee722627695736ee2bc107edf08df978
3.80GHz <shinra.dx@outlook.com>
2024-07-18 12:59:32 +0800

    完善WebSocket调度逻辑

--- app/channels/dispatcher.py
+++ app/channels/dispatcher.py
@@ -1,13 +1,18 @@
 from app.channels.response import response, send
+from app.channels.endpoints import WebSocketEndpoint
+from typing import Dict
 import json
 
 
 def dispatch(ws, raw_data: str):
     data = json.loads(raw_data)
     if data['mode'] == 'request':
-        router(ws, data['endpoint'])
+        router(ws, data['endpoint'], data['data'])
+        return
     send(ws, response('index', {'ping': 'OK!'}))
 
 
-def router(ws, endpoint: str):
-    pass
+def router(ws, endpoint: str, data: Dict):
+    endpoints = WebSocketEndpoint()
+    action = getattr(endpoints, endpoint)
+    action(ws, data)
