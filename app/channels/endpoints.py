0107909f9faab97bf8e4d9259e3a4eda6aa4aff8
3.80GHz <shinra.dx@outlook.com>
2024-07-18 13:15:51 +0800

    添加选手选取图片处理业务端点

--- app/channels/endpoints.py
+++ app/channels/endpoints.py
@@ -1,5 +1,6 @@
 from app.channels.response import response, error, send
 from app.libraries.maimusic import id2music
+from app.libraries.maicover.generator import generate_cover
 from app.libraries.obsws.control import TimingControl
 from app.libraries.constants import MAI_CONST
 from typing import Dict
@@ -11,7 +12,7 @@ class WebSocketEndpoint:
     @staticmethod
     def get_music_info(ws, request: Dict) -> bool:
         if 'music_id' not in request:
-            send(ws, error('get_music_info', f'未指定曲目ID'))
+            send(ws, error('get_music_info', f'请指定曲目ID'))
             return False
         music = id2music(request['music_id'])
         if not music:
@@ -32,11 +33,59 @@ class WebSocketEndpoint:
 
     @staticmethod
     def build_player01_selection(ws, request: Dict) -> bool:
+        if 'music_id' not in request or 'difficulty' not in request:
+            send(ws, error('build_player01_selection', f'请指定曲目ID和难度'))
+            return False
+        if request['difficulty'] > 4:
+            send(ws, error('build_player01_selection', f'难度索引不符合要求，请重新提交'))
+            return False
+        music = id2music(request['music_id'])
+        if not music:
+            send(ws, error('build_player01_selection', f"未找到 ID = {request['music_id']} 的曲目信息"))
+            return False
+        data = {
+            'id': music['id'],
+            'title': music['basic_info']['title'],
+            'artist': music['basic_info']['artist'],
+            'type': music['type'],
+            'level': music['level'],
+        }
+        send(ws, response('build_player01_selection', {
+            'message': '正在处理1P选曲图......'
+        }))
+        generate_cover(MAI_CONST['PLAYER_1P'], data, request['difficulty'])
+        send(ws, response('init_screen', {
+            'message': '1P选曲图生成完成！'
+        }))
         return True
-    
 
+    
     @staticmethod
     def build_player02_selection(ws, request: Dict) -> bool:
+        if 'music_id' not in request or 'difficulty' not in request:
+            send(ws, error('build_player02_selection', f'请指定曲目ID和难度'))
+            return False
+        if request['difficulty'] > 4:
+            send(ws, error('build_player02_selection', f'难度索引不符合要求，请重新提交'))
+            return False
+        music = id2music(request['music_id'])
+        if not music:
+            send(ws, error('build_player02_selection', f"未找到 ID = {request['music_id']} 的曲目信息"))
+            return False
+        data = {
+            'id': music['id'],
+            'title': music['basic_info']['title'],
+            'artist': music['basic_info']['artist'],
+            'type': music['type'],
+            'level': music['level'],
+        }
+        send(ws, response('build_player02_selection', {
+            'message': '正在处理2P选曲图......'
+        }))
+        generate_cover(MAI_CONST['PLAYER_2P'], data, request['difficulty'])
+        send(ws, response('init_screen', {
+            'message': '2P选曲图生成完成！'
+        }))
         return True
 
     
